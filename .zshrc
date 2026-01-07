4c9078caee81e02b86b269f06e34948085887483
cyb3rmutant <yazeed.abu-hummos@proton.me>
2026-01-07 13:26:33 +0000

    deleted everything not needed for the server and edited the init file to work on the server

--- .zshrc
+++ .zshrc
@@ -1,3 +1,5 @@
+ZSH_THEME='zsh-theme-powerlevel10k'
+
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 plugins=(
@@ -14,13 +16,6 @@ source $ZSH/oh-my-zsh.sh
 bindkey '^H' backward-kill-word
 
 alias q='exit'
-alias yaya="yay -Slq | fzf -m --preview 'cat <(yay -Si {1}) <(yay -Fl {1} | awk \"{print \$2}\")' | xargs -ro  yay -S"
-
-alias cf="cd /home/yazeed/projects/cruise-frontend; nvim ."
-alias cb="cd /home/yazeed/projects/cruise-backend; nvim ."
-alias t="cd /home/yazeed/projects/tawfiq-register; nvim ."
-alias z="cd /home/yazeed/projects/ourmedia-html; nvim ."
-alias ly="cd /home/yazeed/projects/l4y; nvim ."
 
 export EDITOR="nvim"
 
@@ -28,11 +23,6 @@ export EDITOR="nvim"
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 export PATH=~/bin:$PATH
 export TERM=xterm-256color
-source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+source ~/powerlevel10k/powerlevel10k.zsh-theme
 
-
-fastfetch --logo arch
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fastfetch
--- init.sh
+++ init.sh
@@ -1,15 +1,7 @@
 # no sudo password
 echo "yazeed ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers >/dev/null
 # install apps
-sudo pacman -S --noconfirm firefox git neovim zsh redshift pulseaudio xorg-xinput unzip kitty spectacle fzf ripgrep npm i3blocks picom man tldr pavucontrol feh ttf-dejavu ttf-liberation brightnessctl fd stow
-
-# yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-rm -rf yay
-yay -S --noconfirm fastfetch zsh-theme-powerlevel10k zsh-syntax-highlighting zsh-autosuggestions clipit-git xkblayout acpi pulseaudio-alsa i3-layouts i3-battery-popup
+sudo dnf install --assumeyes git neovim zsh unzip fzf ripgrep man tldr stow
 
 if [[ -z "expression" ]]; then
 	command ...
@@ -20,20 +12,7 @@ chsh -s $(which zsh)
 sh -c "$(curl -faSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 # configs
-git clone https://github.com/cyb3rmutant/dotfiles
-cd dotfiles
 stow .
-cd
-cp old-stuff/.zsh_histor .
-
-# firefox
-echo export MOZ_USE_XINPUT2=1 | sudo tee /etc/profile.d/use-xinput2.sh
-
-# pacman colorful
-sudo sed -i '/^\[options\]/a Color\nVerbosePkgLists\nParallelDownloads=8\nILoveCandy' /etc/pacman.conf
-
-# grub
-sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
